e69e3b9459526502b03f9fac0dc17dd42f963f6b
Brittany <brittdevelopsit@gmail.com>
2021-05-08 14:40:13 -0700

    Rename component to singular

---
+++ src/components/Plot.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Plot = () => {
+    return (
+        <div
+            style={{
+                width: '100px',
+                height: '100px',
+                border: 'none',
+                textAlign: 'center',
+            }}
+        >
+            plot
+        </div>
+    )
+}
+
+export default Plot;
