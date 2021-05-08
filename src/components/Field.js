59588005ba29401011b19bf2a0df5f72e4836bcf
Brittany <brittdevelopsit@gmail.com>
2021-05-08 12:23:47 -0700

    Set corn, weeds state, pass props to tractor

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tractor from './Tractor';
 
-const Field = () => {
+const Field = ({ tractorPosition }) => {
+    const [corns, setCorns] = useState(2);
+    const [weeds, setWeeds] = useState(1);
+
     return (
-        <div className="field">
-            <Tractor />
+        <div 
+            className="field"
+            style={{
+                position: 'relative',
+                width: '1000px',
+                height: '800px',
+                background: '#fdf5d8',
+                margin: '200px auto',
+                border: '4px solid black',
+            }}
+            >
+            <Tractor tractorPosition={tractorPosition} />
         </div>
     )
 }
