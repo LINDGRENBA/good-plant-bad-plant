899c47efc10762f26b06931ea27f6eb3e35d9f5c
Brittany <brittdevelopsit@gmail.com>
2021-05-08 12:24:08 -0700

    Set tractor position and pass as props

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Field from './components/Field';
 import Header from './components/Header';
 
 const Game = () => {
+  const [tractorPosition, setTractorPosition] = useState([5, 0]);
+
   return (
     <div>
       <Header />
-      <Field />
+      <Field tractorPosition={tractorPosition} />
     </div>
   );
 }
