ab705ca9916d595621b61e1b5a20934f8527cfb7
Brittany <brittdevelopsit@gmail.com>
2021-05-08 20:02:14 -0700

    Add name of game

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,16 +8,22 @@ const Header = ({ profits }) => {
                 padding: '20px',
                 background: '#4c9f70',
                 color: '#fff',
-                fontSize: '20px',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
-                alignItems: 'center'
+                textAlign: 'center',
             }}
         >
-            <button style={{ padding: '20px', fontSize: '30px' }}>Start Game</button>
-            <h2>Mow the grass to increase profit. If you mow the corn, profits will go down!</h2>
-            <h1>Profits: ${profits}</h1>
+            <h1>Good Plant, Bad Plant</h1>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center'
+                }}
+            >
+                <button style={{ padding: '20px', fontSize: '30px' }}>Start Game</button>
+                <h2>Mow the grass to increase profit. If you mow the corn, profits will go down!</h2>
+                <h1>Profits: ${profits}</h1>
+            </div>
         </div>
     )
 }
