a0a53b1571f615cd3cae5d51d975d4bfeba43ea1
Brittany <brittdevelopsit@gmail.com>
2021-05-08 18:04:37 -0700

    Remove grid

--- src/components/Plot.js
+++ src/components/Plot.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const Plot = () => {
-    return (
-        <div
-            style={{
-                width: '100px',
-                height: '100px',
-                border: 'none',
-                textAlign: 'center',
-            }}
-        >
-            {/* plot */}
-        </div>
-    )
-}
+// const Plot = () => {
+//     return (
+//         <div
+//             style={{
+//                 width: '100px',
+//                 height: '100px',
+//                 border: 'none',
+//                 textAlign: 'center',
+//             }}
+//         >
+//             {/* plot */}
+//         </div>
+//     )
+// }
 
-export default Plot;
+// export default Plot;
