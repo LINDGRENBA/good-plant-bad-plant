9ee18d3e2a8a35c18ea476c6fa392a619714c616
Brittany <brittdevelopsit@gmail.com>
2021-05-08 18:39:33 -0700

    Delete the grid / plots for starter template

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Tractor from './Tractor';
-// import Plot from './Plot';
 
 const Field = ({ squares, tractorPosition }) => {
 
@@ -14,13 +13,8 @@ const Field = ({ squares, tractorPosition }) => {
                 background: '#fdf5d8',
                 margin: '100px auto',
                 border: '4px solid black',
-                // display: 'grid',
-                // gridTemplate: 'repeat(8, 1fr) / repeat(10, 1fr)',
             }}
             >
-            {/* {squares.map((square, i) => (
-                <Plot key={i} value={square} />
-            ))} */}
             <Tractor tractorPosition={tractorPosition} />
         </div>
     )
