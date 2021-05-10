6ce04681e189e279132cf6c47a205befc53a3ed4
Brittany <brittdevelopsit@gmail.com>
2021-05-10 09:10:35 -0700

    Update starter template header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,35 @@
 import React from 'react';
 
-const Header = ({ profits }) => {
+const Header = ({ highScore, profits, startNewGame, moves }) => {
     return (
         <div
             style={{
+                position: 'relative',
                 width: '100vw',
-                padding: '20px',
+                padding: '10px',
                 background: '#4c9f70',
                 color: '#fff',
                 textAlign: 'center',
             }}
         >
-            <h1>Good Plant, Bad Plant</h1>
-            <div
+
+            <h2
                 style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-evenly',
-                    alignItems: 'center'
+                    position: 'absolute',
+                    top: '10px',
+                    left: '20px',
                 }}
             >
-                <button style={{ padding: '20px', fontSize: '30px' }}>Start Game</button>
-                <h2>Mow the grass to increase profit. If you mow the corn, profits will go down!</h2>
+                High Score: {highScore}
+            </h2>
+
+            <h1>Good Plant, Bad Plant</h1>
+            <div>
+            <h2>Mow as much grass as you can to increase your profits before you run out of turns.</h2>
+                <h2>Avoid the corn or your profits will take a hit!</h2>
+                <h2>Plants will grow as you move the tractor. Use the arrows on your keyboard.</h2>
                 <h1>Profits: ${profits}</h1>
+                { (moves <= 0) && <button onClick={startNewGame} style={{ padding: '20px', fontSize: '30px' }}>Start New Game</button>}
             </div>
         </div>
     )
