3251c88fc8775168e0233b59d19ed32f2b051b75
Brittany <brittdevelopsit@gmail.com>
2021-05-10 09:11:47 -0700

    Update starter template Plant component.

---
+++ src/components/Plant.js
@@ -0,0 +1,23 @@
+  
+import React from 'react';
+
+const Plant = ({plant}) => {
+    const {x, y, type} = plant;
+    const style = `plant ${type}`;
+    return (
+        <div 
+            className={style}
+            style={{
+                position: 'absolute',
+                top: `${y}px`,
+                left: `${x}px`,
+                height: '100px',
+                width: '100px',
+                backgroundSize: 'cover',
+                zIndex: '10',
+            }}
+        ></div>
+    )
+}
+
+export default Plant;
