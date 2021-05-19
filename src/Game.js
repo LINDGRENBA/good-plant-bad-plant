390b82eda7a756084a8a7a13a239400d2e8c755b
Brittany <brittdevelopsit@gmail.com>
2021-05-18 19:20:40 -0700

    Refactor moveTractor and update function to check bounds.

--- src/Game.js
+++ src/Game.js
@@ -12,14 +12,6 @@ const Game = () => {
   const [plants, setPlants] = useState([]);
   const [highScore, setHighScore] = useState(0);
 
-  const [x, y] = tractorPosition;
-  const directions = {
-    37: [x - 100, y],
-    38: [x, y - 100],
-    39: [x + 100, y],
-    40: [x, y + 100]
-  }
-
   const startNewGame = () => {
 
   }
@@ -28,83 +20,36 @@ const Game = () => {
     
   }
 
-  //example 1 from Andrew
   const moveTractor = e => {
-    const [x, y] = tractorPostion;
-    let newTractorPosition; // left undefined on purpose so that it's falsy
+    e.preventDefault();
+    const [x, y] = tractorPosition;
+    let newTractorPosition; // undefined-falsy
     switch(e.keyCode) {
-      case 37:
+      case 37: // LEFT
         newTractorPosition = [x - 100, y];
+        break;
+      case 38: // UP
+        newTractorPosition = [x, y - 100];
+        break;
+      case 39: // RIGHT
+        newTractorPosition = [x + 100, y];
+        break;
+      case 40: // DOWN
+        newTractorPosition = [x, y + 100];
+        break;
+      default:
+        // no other keys should effect tractor movement
     }
+
     if (newTractorPosition && isInBounds(newTractorPosition)) {
       setTractorPosition(newTractorPosition);
     }
+  }  
+
+  const isInBounds = (newPos) => {
+    const [x, y] = newPos;
+    return (x >= 0 && x <= 1000 && y >= 0 && y <= 800) ? true : false;
   }
-
-  //example 2 from Andrew
-  // based on different concept of checkIfOutOfBounds
-  let isInBounds = checkIfOutOfBounds(tractorPostion);
-
-switch(e.keyCode) {
-	case 37:
-		isInBounds([x - 100, y])
-}
-
-  
-  
-  // rename checkIfOutOfBounds to isInBounds and have it return true or false
-  const moveTractor = (e) => {
-    e.preventDefault();
-    console.log(e.keyCode);
-    const [x, y] = tractorPosition;
-    // create a newTractorPosition copy of the tractorPosition and then when creating a check, pass in the new tractor position
-    // 37 left, 38 up, 39 right, 40 down
-    switch(e.keyCode){
-      // LEFT
-      case 37:
-        // could put unit tests on checkIfOutOfBounds and pass it different values to make sure it's returning the appropriate values
-        checkIfOutOfBounds(tractorPosition)([x - 100, y]);
-        break;
-      // 
-      case 38:
-        checkIfOutOfBounds(tractorPosition)([x, y - 100]);
-        break;
-      case 39:
-        checkIfOutOfBounds(tractorPosition)([x + 100, y]);
-        break;
-      case 40:
-        checkIfOutOfBounds(tractorPosition)([x, y + 100]);
-        break;
-        //if someone presses a diff key, can have a passthrough / no-op
-        // passthrough has to do with switch statement - passes through switch without doing anything
-        // no-op is any function that does no operations
-      default:
-        // no other keys should effect tractor movement
-        // setTractorPosition(tractorPosition);
-    }
-  }
-
-  // double check the logic for newX and newY 
-  // try to see if I can change checkIfOutOfBounds to a curried function - instead of the two functions in moveTractor, move some of the logic down to checkIfOutOfBounds
-  // come to the next meeting with that done, talk about the feedback I received and what I did afterwards 
-  const checkIfOutOfBounds = (tractorPosition) => {
-    //would expect checkIfOutOfBounds to return true or false and then set the tractor position, not for checkIfOutOfBounds to have the side effect of setTractorPosition.
-    return (newPos) => {
-      const [newX, newY] = newPos;
-      // below is a side effect, can't unit test when there are side effects, trickier to test
-      // below is basis of the isInBounds function - anytime a function starts with 'is' you want it to tell you true or false
-      setTractorPosition( (newX >= 0 && newX <= 1000 && newY >= 0 && newY <= 800) ? newPos : tractorPosition );
-      // want a pure function - no side effects and if you give it the same value you get the same value back - idempotent / determinate
-    }
-  }
-
-  // const checkIfOutOfBounds = (e) => {
-  //     return () => {
-  //       const [newX, newY] = directions[e.keyCode];
-  //       console.log( ( (newX >= 0 && newX <= 1000 && newY >= 0 && newY <= 800) ? [newX, newY] : [x, y] ) );
-  //     }
-
-  // }
 
   const checkCoordinates = () => {
     
@@ -118,23 +63,13 @@ switch(e.keyCode) {
     
   }
 
-  useEffect(() => {
-    // 
+  useEffect(() => { 
     window.addEventListener("keydown", moveTractor);
 
-    // could probably remove this, but is protecting against memory leaks if necessary
     return () => {
       window.removeEventListener("keydown", moveTractor)
     }
   }, [tractorPosition]);
-
-  // useEffect(() => {
-  //   window.addEventListener("keydown", checkIfOutOfBounds(tractorPosition));
-
-  //   return () => {
-  //     window.removeEventListener("keydown", checkIfOutOfBounds(tractorPosition))
-  //   }
-  // }, [tractorPosition]);
 
   return (
     <div>
