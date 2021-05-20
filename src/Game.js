8d62e881dc02bb1eb09146ea831a835a47b5b5c5
Brittany <brittdevelopsit@gmail.com>
2021-05-19 19:56:03 -0700

    Add missing t to prop

--- src/Game.js
+++ src/Game.js
@@ -74,7 +74,7 @@ const Game = () => {
   return (
     <div>
       <Header 
-        imeRemaining={timeRemaining} 
+        timeRemaining={timeRemaining} 
         profits={profits}
         startNewGame={startNewGame}
         moves={moves}
